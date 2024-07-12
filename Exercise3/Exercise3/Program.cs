8893b365de7474a05e62eed967fbd00c517e9dea
Arina Pereteatcu <arinapereteatcu0@gmail.com>
2024-07-12 16:14:27 +0300

    Finish Exercise3

---
+++ Exercise3/Exercise3/Program.cs
@@ -0,0 +1,60 @@
+﻿
+using System;
+
+public class Employee
+{
+    public string Name { get; set; }
+    public int ID { get; set; }
+    public int Salary { get; set; }
+
+    public virtual double CalculateAnnualSalary()
+    {
+        return Salary * 12;
+    }
+}
+
+class FullTimeEmployee : Employee
+{
+    public double Bonus { get; set; }
+
+    public override double CalculateAnnualSalary()
+    {
+        return (Salary * 12) + Bonus;
+    }
+}
+
+class PartTimeEmployee : Employee
+{
+    public int HoursWorkedPerMonth { get; set; }
+    public float HourlyRate { get; set; }
+
+    public override double CalculateAnnualSalary()
+    {
+        return HourlyRate * HoursWorkedPerMonth * 12;
+    }
+}
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        FullTimeEmployee fullTimeEmployee = new FullTimeEmployee
+        {
+            Name = "Arina Pereteatcu",
+            ID = 1,
+            Salary = 13000,
+            Bonus = 5000
+        };
+
+        PartTimeEmployee partTimeEmployee = new PartTimeEmployee
+        {
+            Name = "Antonio Margina",
+            ID = 2,
+            HourlyRate = 35,
+            HoursWorkedPerMonth = 80
+        };
+
+        Console.WriteLine($"{fullTimeEmployee.Name}'s annual salary: {fullTimeEmployee.CalculateAnnualSalary()} ");
+        Console.WriteLine($"{partTimeEmployee.Name}'s annual salary: {partTimeEmployee.CalculateAnnualSalary()} ");
+    }
+}
