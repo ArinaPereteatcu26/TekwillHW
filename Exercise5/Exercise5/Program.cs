7dfd51ef2ac3da44eada0c7b1be68718cc120543
Arina Pereteatcu <arinapereteatcu0@gmail.com>
2024-07-12 16:17:01 +0300

    Finish Exercise5

---
+++ Exercise5/Exercise5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+public static class Helper
+{
+    public static Random random = new Random();
+    public static string GenerareCodUnic(string prefix)
+    {
+        int randomNumber = random.Next(1000, 9999);
+        return $"{prefix}{randomNumber}";
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(Helper.GenerareCodUnic("ABC"));
+        Console.WriteLine(Helper.GenerareCodUnic("XYZ"));
+        Console.WriteLine(Helper.GenerareCodUnic("MIAU"));
+        Console.WriteLine(Helper.GenerareCodUnic("ABC"));
+        Console.WriteLine(Helper.GenerareCodUnic("ABC"));
+        Console.WriteLine(Helper.GenerareCodUnic("ABC"));
+    }
+}
