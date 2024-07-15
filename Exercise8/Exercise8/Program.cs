79ebda67271d70e1c000af53596bd1e3d2cde688
Arina Pereteatcu <arinapereteatcu0@gmail.com>
2024-07-15 14:26:00 +0300

    Finish exercise8

---
+++ Exercise8/Exercise8/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+public interface IRunnable
+{
+    void Run();
+}
+
+public abstract class Animal
+{
+    public abstract void MakeSound();
+
+    public virtual void Eat()
+    {
+        Console.WriteLine("Animal eats");
+    }
+}
+
+public class Caine : Animal, IRunnable
+{
+    public override void MakeSound()
+    {
+        Console.WriteLine("Caine latra");
+    }
+    public override void Eat()
+    {
+        Console.WriteLine("Cainele mananca oase");
+    }
+
+    public void Run()
+    {
+        Console.WriteLine("Dog runs");
+    }
+}
+
+public class Pisica : Animal, IRunnable
+{
+    public override void MakeSound()
+    {
+        Console.WriteLine("Cat miau");
+    }
+
+    public override void Eat() 
+    {
+        Console.WriteLine("Pisica mananca peste");
+    }
+    public void Run()
+    {
+        Console.WriteLine("Cat runs");
+    }
+}
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Caine caine = new Caine();
+        caine.MakeSound();
+        caine.Eat();
+        caine.Run();
+
+        Pisica pisica = new Pisica();
+        pisica.MakeSound();
+        pisica.Eat();
+        pisica.Run();
+
+    }
+}
