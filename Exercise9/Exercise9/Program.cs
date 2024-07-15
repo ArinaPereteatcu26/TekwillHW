a7d0edd39c76a5839aecc808ac017ad7befd0219
Arina Pereteatcu <arinapereteatcu0@gmail.com>
2024-07-15 15:18:11 +0300

    Finish exercise9

---
+++ Exercise9/Exercise9/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+public class Angajat
+{
+    public string Nume;
+
+    public virtual void AfisareDetalii()
+    {
+        Console.WriteLine($"Numele este:{Nume}");
+    }
+
+    public Angajat(string nume) 
+    {
+        Nume= nume;
+    }
+}
+
+ class Manager : Angajat
+{
+    public string Departament;
+    
+    public override void AfisareDetalii()
+    {
+        base.AfisareDetalii();
+        Console.WriteLine($"Departamentul este: {Departament}");
+    }
+
+    public Manager(string nume, string departament) : base(nume)
+    {
+        Departament= departament;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Manager manager = new Manager("Travis Scott", "Music");
+
+        manager.AfisareDetalii();
+    }
+}
