7ac509cc1050c562d8d750bd57c9eda07726727b
Arina Pereteatcu <arinapereteatcu0@gmail.com>
2024-07-28 16:34:15 +0300

    Finish exercise16

---
+++ SOLIDprinciples/SOLIDprinciples/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+public class Order
+{
+    public int Id { get; set; }
+    public double Amount { get; set; }
+}
+
+public interface IProcessPayment
+{
+    void ProcessPayment(Order order);
+}
+
+public class CreditCardPaymentProcess : IProcessPayment
+{
+    public void ProcessPayment(Order order)
+    {
+        Console.WriteLine("Processing payment");
+    }
+}
+
+public class PayPalPaymentProcess : IProcessPayment
+{
+    public void ProcessPayment(Order order)
+    {
+        Console.WriteLine("Processing Paypal payment");
+    }
+}
+
+public class BankTransferPaymentProcess : IProcessPayment
+{
+    public void ProcessPayment(Order order)
+    {
+        Console.WriteLine("Processing bank transfer");
+    }
+}
+
+public class DataBase
+{
+    public void SaveToDataBase(Order order)
+    {
+        Console.WriteLine("Saving order to database");
+    }
+
+    public Order LoadFromDataBase(int orderID)
+    {
+        Console.WriteLine("Loading order from database");
+        return new Order();
+    }
+}
