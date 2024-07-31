a95118f1a78a4da6c13d71e05074fe52a33bd13d
Arina Pereteatcu <arinapereteatcu0@gmail.com>
2024-07-31 19:57:18 +0300

    Finish exercise

---
+++ SOLIDprinciples2/SOLIDPrinciples2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+
+public interface IWorkable
+{
+    void Work();
+}
+
+public interface IEatable
+{
+    void Eat();
+}
+
+public class HumanWorker : IWorkable, IEatable
+{
+    public void Work()
+    {
+        Console.WriteLine("Human working...");
+    }
+
+    public void Eat()
+    {
+        Console.WriteLine("Human eating...");
+    }
+}
+
+public class RobotWorker : IWorkable
+{
+    public void Work()
+    {
+        Console.WriteLine("Robot working...");
+    }
+}
+
+public class WorkerManager
+{
+    private readonly IWorkable _workable;
+
+    public WorkerManager(IWorkable workable)
+    {
+        _workable = workable;
+    }
+
+    public void Manage()
+    {
+        _workable.Work();
+    }
+}
