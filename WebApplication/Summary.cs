da7cab70b66cc8bf5abab9cfa0d4237c3755fd13
Arina Pereteatcu <arinapereteatcu0@gmail.com>
2024-08-14 23:52:44 +0300

    Add Postman collections

---
+++ WebApplication/Summary.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplication.Models
+{
+	public class Summary
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+
+
+	}
+}
--- WebApplication/WebApplication/Controllers/WeatherForecastController.cs
+++ WebApplication/WebApplication/Controllers/WeatherForecastController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.FileProviders;
 
 namespace WebApplication.Controllers
 {
@@ -6,9 +7,13 @@ namespace WebApplication.Controllers
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
+        private static List<WeatherForecast> Summaries = new List<WeatherForecast>
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            new WeatherForecast { Date = DateTime.Now.AddDays(1), TemperatureC = 25, Summary = "Hot"},
+            new WeatherForecast { Date = DateTime.Now.AddDays(2), TemperatureC = 15, Summary = "Cold"},
+            new WeatherForecast { Date = DateTime.Now.AddDays(3), TemperatureC = 20, Summary = "Warm"},
+            new WeatherForecast { Date = DateTime.Now.AddDays(4), TemperatureC = 30, Summary = "Very Hot"},
+            new WeatherForecast { Date = DateTime.Now.AddDays(5), TemperatureC = 10, Summary = "Very Cold"}
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
@@ -19,15 +24,85 @@ namespace WebApplication.Controllers
         }
 
         [HttpGet(Name = "GetWeatherForecast")]
-        public IEnumerable<WeatherForecast> Get()
+        public IEnumerable<WeatherForecast> GetAll()
         {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            return Summaries;
+        }
+
+        [HttpGet("{summary}", Name = "GetWeatherForecastByName")]
+        public IActionResult GetByName(string summary)
+        {
+            var weather = Summaries.FirstOrDefault(x => x.Summary == summary);
+            if (weather == null)
             {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
+                return NotFound();
+            }
+            return Ok(weather);
+        }
+
+        [HttpPost(Name = "CreateWeatherForecast")]
+        public IActionResult Create(WeatherForecast weather)
+        {
+            var weatherFromList = Summaries.FirstOrDefault(x => x.Summary == weather.Summary);
+            if (weatherFromList == null)
+            {
+                Summaries.Add(weather);
+                return CreatedAtRoute("GetWeatherForecastByName", new { weather.Summary }, weather.Summary);
+            }
+            return BadRequest("Already Exists");
+        }
+
+        [HttpPost("CreateMultiple", Name = "CreateMultipleWeatherForecasts")]
+        public IActionResult CreateMultiple(List<WeatherForecast> weatherForecasts)
+        {
+            var addForecasts = new List<WeatherForecast>();
+            var existingForecasts = new List<string>();
+
+            foreach (var weather in weatherForecasts)
+            {
+                if (Summaries.Any(x => x.Summary == weather.Summary))
+                {
+                    existingForecasts.Add(weather.Summary);
+                }
+                else 
+                {
+                    Summaries.Add(weather);
+                    addForecasts.Add(weather);
+                }
+            }
+            if (existingForecasts.Count > 0)
+            {
+                return BadRequest($"Forecast exists");
+            }
+            return Ok(addForecasts);
+        }
+
+        [HttpDelete("{summary}", Name = "DeleteWeatherForecastByName")]
+        public IActionResult DeleteByName(string summary)
+        {
+            var weather = Summaries.FirstOrDefault(x => x.Summary == summary);
+            if (weather == null)
+            {
+                return NotFound();
+            }
+            Summaries.Remove(weather);
+
+            return Ok();
+        }
+
+        [HttpPut("{summary}", Name = "UpdateWeatherByName")]
+        public IActionResult UpdateWeather(string summary, WeatherForecast weatherForecast)
+        {
+            var index = Summaries.FindIndex(x => x.Summary == summary);
+            if (index == -1)
+            {
+                return NotFound();
+            }
+            Summaries[index].Date = weatherForecast.Date;
+            Summaries[index].TemperatureC = weatherForecast.TemperatureC;
+            
+
+            return Ok();
         }
     }
 }
